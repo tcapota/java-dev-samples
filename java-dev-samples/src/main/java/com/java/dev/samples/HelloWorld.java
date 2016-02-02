8f9c465d10548a8f631e22766931e2c6ebfbf8a9
tcapota <timcapota@gmail.com>
2016-02-01 19:50:13 -0600

    Added Basic Maven Project

---
+++ java-dev-samples/src/main/java/com/java/dev/samples/HelloWorld.java
@@ -0,0 +1,21 @@
+/**
+ * 
+ */
+package com.java.dev.samples;
+
+/**
+ * @author tim
+ *
+ */
+public class HelloWorld {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hello World!");
+
+	}
+
+}
